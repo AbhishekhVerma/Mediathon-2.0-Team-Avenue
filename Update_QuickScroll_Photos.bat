550ff5efd170c7d58c90cc88dfba48e082d5a719
Abhishekh Verma <abhishekh_verma2006@live.com>
2026-09-26 11:04:43 +0400

    Remove bat file, implement dynamic numerical photo loading for QuickScroll_Sequence, reorder layout

--- Update_QuickScroll_Photos.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Scanning QuickScroll_Sequence for new JPGs...
-powershell -ExecutionPolicy Bypass -Command "$photos = Get-ChildItem 'QuickScroll_Sequence' -Filter *.JPG | Sort-Object Name; $urls = @(); foreach ($p in $photos) { $urls += '\"QuickScroll_Sequence/' + $p.Name + '\"' }; $json = '[' + ($urls -join ',') + ']'; $html = Get-Content index.html -Raw; $html = $html -replace 'data-images=''\[\^'']*QuickScroll_Sequence.*?\]''', ('data-images=''' + $json + ''''); $html | Out-File index.html -Encoding utf8"
-echo Done! index.html has been updated to use your new photos.
-pause
